1c9fc91680924512faa8ac5d83e1de82b8ef7b69
lestersinjacreater <lestetsinjawatosia@gmail.com>
2024-06-14 15:50:48 +0300

    fixed

--- src/drizzle/migrations/0003_fixed_misty_knight.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "users" ALTER COLUMN "phone_verified" DROP DEFAULT;--> statement-breakpoint
-ALTER TABLE "users" ALTER COLUMN "email_verified" DROP DEFAULT;--> statement-breakpoint
-ALTER TABLE "users" ALTER COLUMN "restorant_owner" SET DATA TYPE boolean;
